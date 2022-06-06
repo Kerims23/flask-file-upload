76b4ddf9079b3be825e9a8456bd15013482eabbc
Kerim Sever <ks835@njit.edu>
2022-06-06 08:40:31 -0400

    comments and changes

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
+#pip install flask 
+#pip install flask_wtf wtforms
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -18,10 +21,14 @@ class UploadFileForm(FlaskForm):
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        file = form.file.data 
+        # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) 
+        # Then save the file
         return "File has been uploaded."
+        # Need to add display file as next step
     return render_template('index.html', form=form)
+    # this should upload it into templates folder on your computer
 
 if __name__ == '__main__':
     app.run(debug=True)
